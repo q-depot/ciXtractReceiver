2ebb8112d4c218d57664339e0b4f4378a65d9c67
Q <andrea@pushthemonkey.com>
2013-07-21 20:11:58 +0100

    ..

--- templates/ciXtractReceiver/src/_TBOX_PREFIX_App.cpp
+++ templates/ciXtractReceiver/src/_TBOX_PREFIX_App.cpp
@@ -1,7 +1,7 @@
 
 #include "cinder/app/AppNative.h"
 
-#include "ciXtract.h"
+#include "ciXtractReceiver.h"
 
 using namespace ci;
 using namespace ci::app;
@@ -19,81 +19,40 @@ public:
 	void update();
 	void draw();
 	
-	void drawPcmData();
-	
+    ciXtractReceiverRef                 mXtractReceiver;
 
-    audio::Input                    mInput;
-    ciXtractRef                     mXtract;
-	
+    FeatureDataRef 						mSpectrumData;
 };
 
 
 void _TBOX_PREFIX_App::prepareSettings(Settings *settings)
 {
-	settings->setTitle("ciXtract Sample");
+	settings->setTitle("ciXtract OSC Receiver");
 	settings->setWindowSize( 1280, 720 );
 }
 
 
 void _TBOX_PREFIX_App::setup()
 {
-    const std::vector<audio::InputDeviceRef>& devices = audio::Input::getDevices();
-	for( std::vector<audio::InputDeviceRef>::const_iterator iter = devices.begin(); iter != devices.end(); ++iter )
-    {
-        if ( (*iter)->getName() == "Soundflower (2ch)" )
-        {
-            mInput = audio::Input( *iter );
-            mInput.start();
-            break;
-        }
-	}
- 
-    if ( !mInput )
-        exit(-1);
- 
-    mXtract = ciXtract::create( mInput );
+    mXtractReceiver = ciXtractReceiver::create();
+	
+	mSpectrumData = mXtractReceiver->getFeatureData( "spectrum" );
 }
 
 
 void _TBOX_PREFIX_App::update()
 {
-	mXtract->update();	
+	
 }
 
 
 void _TBOX_PREFIX_App::draw()
 {
-	gl::clear( Color::gray( 0.85f ) ); 	
+	gl::clear( Color::gray( 0.85f ) );
 	
-	gl::color( Color::gray( 0.1f ) );
-	drawPcmData();
+
 }
 
-
-void _TBOX_PREFIX_App::drawPcmData()
-{
-    audio::PcmBuffer32fRef pcmBuffer = mInput.getPcmBuffer();
- 
-	if( !pcmBuffer )
-		return;
- 
-	uint32_t bufferLength           = pcmBuffer->getSampleCount();
-	audio::Buffer32fRef leftBuffer  = pcmBuffer->getChannelData( audio::CHANNEL_FRONT_LEFT );
- 
-	int     displaySize = getWindowWidth();
-	float   scale       = displaySize / (float)bufferLength;
-
-	PolyLine<Vec2f>	leftBufferLine;
-
-	for( int i = 0; i < bufferLength; i++ )
-    {
-		float x = i * scale;
-        float y = 50 + leftBuffer->mData[i] * 60;
-		leftBufferLine.push_back( Vec2f( x , y) );
-	}
-
-	gl::draw( leftBufferLine );
-}
 
 
 CINDER_APP_BASIC( _TBOX_PREFIX_App, RendererGl )
